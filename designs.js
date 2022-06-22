cd139e784fb39e563504ac4e2e31388d1e27c17e
Constant Dauphin <constantdauphin@macbook-pro-1.home>
2022-06-22 18:20:47 +0200

    firstCommit

---
+++ designs.js
@@ -0,0 +1,23 @@
+// Select color input
+let color = document.getElementById("colorPicker").value;
+console.log(color);
+// Select size input
+let height = document.getElementById("inputHeight").value;
+console.log(height);
+let width = document.getElementById("inputWidth").value;
+console.log(width);
+// When size is submitted by the user, call makeGrid()
+
+const form = document.getElementById("sizePicker");
+console.log(form);
+
+/*form.addEventListener("submit", (event) => {
+    console.log(event);
+});*/
+
+function makeGrid() {
+
+// Your code goes here!
+
+    console.log("hello");
+}
