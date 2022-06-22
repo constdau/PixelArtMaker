d54abdf87555a12ab4ac2bed1c8a5534c11c510a
Constant Dauphin <constantdauphin@macbook-pro-1.home>
2022-06-22 19:29:02 +0200

    edit to design.js

--- designs.js
+++ designs.js
@@ -1,23 +1,53 @@
-// Select color input
-let color = document.getElementById("colorPicker").value;
-console.log(color);
-// Select size input
-let height = document.getElementById("inputHeight").value;
-console.log(height);
-let width = document.getElementById("inputWidth").value;
-console.log(width);
+// Global variables
+const colorPicker = document.getElementById("colorPicker");
+const form = document.getElementById("sizePicker");
+
+var canvas = document.getElementById("pixelCanvas");
+var color = colorPicker.value;
 // When size is submitted by the user, call makeGrid()
 
-const form = document.getElementById("sizePicker");
-console.log(form);
-
-/*form.addEventListener("submit", (event) => {
-    console.log(event);
-});*/
-
-function makeGrid() {
-
-// Your code goes here!
-
-    console.log("hello");
+function makeGrid(height, width) {
+    // Clear table
+    while (canvas.firstChild) canvas.removeChild(canvas.firstChild);
+    // Create canvas
+    for (let i = 0; i < height; i++) {
+        let row = canvas.insertRow();
+        for (let j = 0; j < width; j++) {
+            let col = row.insertCell();
+        }
+    }
 }
+
+// Main
+// Get Height and Width, create Canvas
+form.addEventListener("submit", (event) => {
+    // Stop page refresh
+    event.preventDefault();
+    // Select width and height from DOM
+    let height = document.getElementById("inputHeight").value;
+    let width = document.getElementById("inputWidth").value;
+    // Create grid
+    makeGrid(height, width);
+});
+
+// Select color
+colorPicker.addEventListener("change", () => {
+    color = colorPicker.value;
+});
+
+// Color the canvas
+canvas.addEventListener("click", (event) => {
+    // Select cell
+    let cell = event.target.closest("td");
+    
+    // If no cell clicked
+    if (!cell) {return;}
+    
+    // Color the cell
+    if (!cell.style.backgroundColor){
+        cell.style.backgroundColor = color;
+    }
+    else {
+        cell.style.backgroundColor = null;
+    }
+});
